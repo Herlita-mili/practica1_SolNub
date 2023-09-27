581e0b620a5625d8ff3bde8820b3708e36e51760
herlita <herlita.farro@tecsup.edu.pe>
2023-09-26 22:57:53 -0500

    first commit

--- base_de_datos.php
+++ base_de_datos.php
@@ -1,9 +1,9 @@
 <?php
-$contraseña = "LWQFEwchxBkkWxFscK3q";
+$contraseña = "jHamKPupIEe240D2wfef";
 $usuario = "postgres";
 $nombreBaseDeDatos = "railway";
-$rutaServidor = "containers-us-west-84.railway.app";
-$puerto = "6644";
+$rutaServidor = "containers-us-west-58.railway.app";
+$puerto = "6996";
 try {
  $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
  $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
